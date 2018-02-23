36ce3560d8e2d49d08cb53e89dd25f0a423fc885
Thomas Yoho <TYohoJr@Gmail.com>
2018-02-23 15:26:06 -0700

    hmmm wut?

--- mongo2.js
+++ mongo2.js
@@ -9,30 +9,43 @@ var db
 
 MongoClient.connect('mongodb://localhost:27017/moby', (err, client) => {
   if (err) return console.log(err)
-  db = client.db('star-wars-quotes') // whatever your database name is
+  db = client.db('moby') // whatever your database name is
   app.listen(3000, () => {
     console.log('listening on 3000')
   })
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index2.html')
-  })
+    res.sendFile(__dirname + '/index2.html');
+});
 
-app.post('/quotes', (req, res) => {
-  db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err)
+// Add data to the database
+app.post('/users', (req, res) => {
+  db.collection('users').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+    console.log('saved to database');
+    res.redirect('/');
+  });
+});
 
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
-
+// Delete data in the database
 app.post('/delete', (req, res) => {
-    db.collection("quotes").remove(
+    db.collection('users').remove(
         req.body, (err, result) => {
-            if (err) return console.log(err)
-            console.log('saved to database')
-            res.redirect('/')
-        })
-})
+            if (err) return console.log(err);
+            console.log('saved to database');
+            res.redirect('/');
+        }
+    );
+});
+
+// Update data in the database
+app.post('/update', (req, res) => {
+    db.collection('users').update(
+        {username:req.body.username, password:req.body.password, email:req.body.email}, {username:req.body.username2, password:req.body.password2, email:req.body.email2}, (err, result) => {
+            if (err) return console.log(err);
+            console.log('updated in database');
+            res.redirect('/');
+        }
+    );
+});
