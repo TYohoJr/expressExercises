ca17dc789358117e24738dfcdfcf27ca6d61276f
Thomas Yoho <TYohoJr@Gmail.com>
2018-02-26 09:24:12 -0700

    .

--- proxy.js
+++ proxy.js
@@ -4,3 +4,15 @@
 //Use postman to grab and view the data. 
 
 //This is what a proxy server is - a server that acts as a middleman - in this case it is between openWeather and a client.
+
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+
+app.listen(5000, function() {
+    console.log("Listening on 5000");
+});
